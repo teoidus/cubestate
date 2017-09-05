ce546f2d7a065bf87dae31895f8083b66e598c6d
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-05 12:24:24 -0400

    Update test.js

--- test.js
+++ test.js
@@ -5,10 +5,3 @@ var compiler = new CubestateCompiler();
 
 console.log(compiler.compile(fs.readFileSync("helloworld.cs").toString()));
 console.log(compiler.compile(fs.readFileSync("cat.cs").toString()));
-
-const Cube = require("./cube.js");
-
-var cube = new Cube();
-
-cube.apply("R U R' U' R' F R2 U' R' U' R U R' F'");
-console.log(cube.state.join("").match(/.{9}/).join("\n"));
