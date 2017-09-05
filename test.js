e32bf17167f6f16cb2e3cdec91a2bc145dc8cbe0
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-05 13:49:15 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const cubestate = require("./cubestate.js");
 const fs = require("fs");
 
+console.log(cubestate.compile(fs.readFileSync("helloworld.cs").toString()));
 console.log(cubestate.compile(fs.readFileSync("cat.cs").toString()));
