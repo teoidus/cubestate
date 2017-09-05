2b89bcef047eae614f3a51df9b5b14ea58b3f8b3
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-05 16:33:30 -0400

    Update chunk-bf.js

--- util/chunk-bf.js
+++ util/chunk-bf.js
@@ -1,16 +1,18 @@
 module.exports = function chunkBF (code) {
 	var chunked = [];
+	var offset = 0;
 	
 	code = code.replace(/[^+\-><\[\].,]/g, "");
 	
 	for (var i = 0; i < code.length; i ++) {
-		if (i % 9 == 0) {
+		if ((i - offset) % 9 == 0) {
 			chunked.push("");
 		}
 		
 		chunked[chunked.length - 1] += code[i];
 		
 		if (code[i] == "." || code[i] == ",") {
+			offset += (i - offset) % 9
 			chunked.push("");
 		}
 	}
