355b82bbd1728542492c50c89dfcde7f8aa295f9
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-05 10:36:07 -0400

    Create helloworld.cs

---
+++ helloworld.cs
@@ -0,0 +1,18 @@
+R' D R
+R' D' E R2 E' L' E R'
+M E' M' E' U' B M B' M' D L D L'
+r2 R' U R U R' U2 M D' M' F' D2 F L' D L D S D2 S'
+M2 B U L' B' D B M F' M' F L E' L' D' F R' D R D' M D2 M'
+E2 F D B L E L' .6
+L U2 B' U' B2 .4
+M' F E R2 E2 L' F' D' F B' D' B .7
+.0
+.3
+B .2
+F L B' .2
+B .1
+F' U2 .3
+x2 F U' R D' B U' .6
+F U D L' U2 .8
+R' D2 B .3
+M .3
