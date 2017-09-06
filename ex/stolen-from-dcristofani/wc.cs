1a61c2932141b45f0c14375d1dd6d62e642b8a14
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:49:04 -0400

    Create wc.cs

---
+++ ex/stolen-from-dcristofani/wc.cs
@@ -0,0 +1,37 @@
+r E2 L'
+F L F D2 F'
+L B' ,2
+U S L B2 M2 D' B
+L' S R2 D' F' M' U2
+F R F' D2 M' B2
+D' F E2 S B' L'
+B' S' M2 D' F2 L'
+R' F2 D S' B2
+R U R2 D R' S
+R' U' F' D R2 S
+U L2 F' U F U2 F
+B U M2 D R
+U B' U2 L S' L'
+D' F' U F' D2 M2
+M U' B M' D' L
+E F U2 S' L M2
+L U M S B' D2 R'
+L B' E' D2 M
+S' R' B U2 D2 L
+B' U2 S' D L2 F2
+D R2 E' M' S2 U2 L'
+R S B' R S2 L2
+D M2 B' D L' R' S2
+R L2 D' L M2
+M' D M D L2 S2
+U' E' S2 U' B' R'
+D B2 L' B R2 F2
+U2 ,3
+B S2 L' F M B2
+L' D2 F L R2
+D' R' S E U2 S
+B2 D2 R U B M
+B2 U S M' .5
+U F L' U E R'
+U R U S2
+6
