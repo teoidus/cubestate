c26342d4cb4058062d2fc6d826cb39cf8fa3e7f7
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:46:12 -0400

    Create digitalroot.cs

---
+++ ex/stolen-from-keymaker/digitalroot.cs
@@ -0,0 +1,17 @@
+L' ,1
+B E' L2 U R'
+R D2 l2 B' R
+E R' U S' F2 L2 R2
+L U F U F M' S'
+M L' S' U F R'
+S R' E' L2 U R'
+E' L' F R' U M' R2
+D' R F B E2 R'
+F2 D' R' F' L2
+D F B S2 R M F'
+,0
+L' U2 S2 M' U R'
+B' L U B L'
+L U B' L'
+U' S .4
+U L' .1
