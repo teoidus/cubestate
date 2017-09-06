5863da2dca7fb0953e5c19b3621694bb07ca8039
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:09:43 -0400

    Create squares.cs

---
+++ ex/stolen-from-dcristofani/squares.cs
@@ -0,0 +1,23 @@
+L E' L' E S
+M2 B U F R'
+R' U' M B' U F'
+D B' L' S2 L' S2
+F L E' R2 D2 M'
+U R' U M2 F R2 B2
+R U L2 B2 R' S2
+F B U F' D' B
+D' B' R E2 F' L2
+B2 L S r2
+S2 D2 S L' U' L'
+U F2 L U2 L2 .6
+D B S2 L B' D F
+U M B L B2 L2 B2
+D B' R F' D' S2 L
+R S B2 L' F M B
+L S U2 R' B' L2
+R M S' E' B L
+E L r S2
+L' F' M B' L2 S'
+F S2 D2 R' F M2
+R S' M2
+4
