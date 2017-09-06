7d50aa974f2e1fb9ed4f8b7773d96154290b3c73
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:43:30 -0400

    Create fib.cs

---
+++ ex/stolen-from-dcristofani/fib.cs
@@ -0,0 +1,21 @@
+B D' B'
+R' E L F R S'
+M' D' F R D2 F'
+B L' U2 S R
+.0
+U L E' F' L2 D L'
+B L2 E' D2 R F S2
+F U' L S' B2 M .8
+F E S M' B' D' L2
+R B S L' D2 F
+S' L2 U M2 S2 R'
+D S' R' M2 S2 R2
+F2 R L' D2 S M'
+M D S2 U2 L' F2
+L E R D L2 B' M
+U F M S' B2 L' S
+U B2 L E2 B' R'
+R L' U' F2 D L
+F' L' U M S' U' B'
+L F' B' R' F2 L R2
+9
