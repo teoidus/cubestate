b1b5f076e950af3934eae4174914f7738d6a9cf7
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:44:07 -0400

    Create numwarp.cs

---
+++ ex/stolen-from-dcristofani/numwarp.cs
@@ -0,0 +1,83 @@
+L E B F' R L'
+L E M B' L' F'
+D' F' L2 ,5
+U' M' D' U2 L
+U L F d L' S2
+L U2 M2 F' S2 R
+B' U' L F' B2
+B F' E' M' U' B S2
+U M U' F B2 M
+R F L' S U D2 L
+L R' S U' E' F'
+B' L D B S2
+U B M D' S F' R2
+L' F E B M
+E L2 E2 S L F'
+M B' U D M B2
+U' D' F M2 D L'
+M D2 M2 U L
+D R' U' F' M'
+M F' U' F2 L B' R2
+F U' E' M' B2 R'
+R U' D' M S M2
+L R' B2 U' S D2 F
+D M2 B2 R2 D' L
+L F L2 B2 D M2 S'
+F' D2 R2 S' U B'
+R M F' U L2 F2
+D L2 D2 S' L E' F'
+E' R2 F L2 S2
+L' D' F' D' M L2
+U M B' R2 S M' B'
+M' D E2 S R' B'
+U R E S D F
+L S' R2 U L D2 B
+B2 M R2 D' S2 M
+B L' U' S' M' U' F
+F M S2 U' R2 F
+L F2 M2 B2 D2 S2
+B D L2 U2 D' B
+U L B R2 U' L B'
+U' S M2 B' D2 L
+D' U2 F' M B' L2
+B M L' U2 R S'
+B2 U F2 r2
+l E2 R S' R2 B'
+R' M' U' B' R' B2
+M F2 D2 L M'
+U F2 L D F2 B2
+B' U' B' D2 M
+L D2 L F B' E' F
+D R' B' E' F R
+U M' E' L2 F2 U2 R
+R D' S F' L2 B2 R2
+F R2 F' l B S2
+
+F S2 D L' B U' B2
+U S' E' L2 F' R2 B
+F M U' E' S' F2
+F' M' R2 U M' F2
+D U2 S' F2 R'
+U S' E R B' D2 L
+R' F L' E' M U2 F
+B L' E R2 F' B2 M'
+E R2 U S
+F U B2 D' L' R2
+M' L2 D' F B2 M
+R S D2 R F' B2 L'
+F U2 .2
+U R' M' S2
+F L R' F2 U2 S2 L
+B D U' L B' R2 .8
+U S2 L D R' F' R'
+U' D2 R' M2 D' B2
+L F U M S D' L
+F R2 B L E2 L M'
+S r F2 L2 S2
+U D' B' U2 M' B' R'
+U L2 B U' L' S
+L' E R2 B S M2
+S' R2 B L .5
+U R B2 M F' L' S
+E B
+2
