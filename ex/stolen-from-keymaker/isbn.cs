a6f135fe7a3e161897ec0c01820c8e6b4c3e0bf2
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 14:00:57 -0400

    Create isbn.cs

---
+++ ex/stolen-from-keymaker/isbn.cs
@@ -0,0 +1,94 @@
+S F2 M' U' F' ,7
+M S' R2 U F
+M L2 B D M R2 B
+U B E' R S R B2
+U' R' S2 ,4
+D2 S' B2 R2 D L2
+F L2 D2 M2 D' R' B
+L F' L2 U D2 S M2
+R U' R2 S2 ,5
+U D2 F R2 U' L S'
+F U2 S R' F2 L2 S
+U F' E L2 F2 r
+U S U' S ,6
+U2 M' F' D L
+R' S' U B D' S' L
+f M' D R2
+D B L2 E2 S' ,7
+F M2 S2 L R' B
+L E U2 B' D' F S2
+F D2 R' S2 D M F2
+B F2 D L2 B2 ,8
+L M' B2 U2 B' R2
+u F' B2 R S2 L
+U S' R2 E2 F M2
+U' M' F2 L2 U' F
+,0
+E' L2 F2 U' F M
+R F S' D' M' B' S'
+L D' R U2 S' M F'
+R U2 L S L2 F'
+B' ,1
+D2 B U' B' D M2
+M2 U B2 R B L F
+L' B2 E2 S L2 F'
+U' L M2 U' F
+l ,2
+U S E' B' D' F2 L
+D F' U2 S' l S'
+U' L' S' M' R2 B2
+U E F' M' R2 D M2
+F M S D2 r2
+F R2 S' R' F'
+U M D' B2 R' B R'
+S U' F2 U B' D2 F
+M' S L2 F D' B' R2
+D R F' D B S
+U R' D' B U2 S M'
+M U' R' D' S
+L' F' R2 D' L ,8
+R D S E L' M2 B
+D2 F' R u B' S2
+U S' E' R E2 B L'
+U' B U' M' S
+R F' D' B R' M' S'
+U B2 U2 L2 D' R2 S
+L S U R' F D B
+U S u L2 U' B
+L E M' U' L2 D2 B
+D2 B R' U R2 S2
+E' R2 S' M R' B
+U' M' F2 D' L B2
+U2 D2 F' S2 R'
+B' R M S'
+L R F2 D U2 S M2
+L D' R' B M' F S2
+R2 D L S2 M
+L F E S D' F'
+R S B2 L2 U' F'
+S' D2 L' D' F2
+M D' B' U2 M' B'
+F R' S' F2 R
+U B U S2 D F'
+U' B R' B'
+R B R .4
+B' U .5
+U S' M U S M
+B' E2 R U M F2
+F R' D' F U L'
+R L' D2 B S2 .7
+S' U' M S' L2
+D R' B D B S2 .8
+U' F D' S2 U R F2
+.2
+L' U R' .3
+D2 S2 .5
+D R' S2 R2 U' L
+U2 .4
+B M' U2 M2 S2 ,8
+B' U' L' U2 L2 D B
+L F' D' R' F2 M B2
+M L' U R2 S D2 F
+R' F2 D2 S' U R
+F R2 B
+7
