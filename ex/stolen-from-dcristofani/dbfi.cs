df8987807d93b4f49b419883415160f7823c3a1b
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:05:44 -0400

    Create dbfi.cs

---
+++ ex/stolen-from-dcristofani/dbfi.cs
@@ -0,0 +1,50 @@
+R U' E' D2 R' F' M
+D M' S2 U2 D M2
+E U2 L R U' R
+R B M B' R'
+D F' L2 F2 B2 L2
+M' U B E L'
+U2 S' R S
+S L U F' ,7
+L S2 E' R2 B' S'
+L B' E' M' F2 L2
+F L D2 F2 D S2
+E B' L2 S2 L2
+U B' l' U' R' F
+D2 L2 S M2 F
+U' B L' F' E2 R' F'
+u F' D2 F M
+B E U' S M D2 B
+U F' R F' E S'
+L M2 U' R' B2
+R F L S2 U2 D L'
+U L' U' L U2 L F
+S E B M2 D2 R2
+F E M2 U' L' E B
+U R' D2 M' B R' L2
+U S L U' R S2 L'
+D B E2 B
+u R2 S' L2 B'
+U L U D' L
+U E B' D2 M R2 S2
+D' F r S' F2
+U D' B S D' R2 S2
+U R U2 F' D' R S
+U S' D' F2 D R L2
+F' D' B' S2 L' F'
+U' L2 U E2 M'
+F M' U R2 B R' B2
+B M' B2 D' S' L F2
+E B L B' L E' F
+L R S2 U' D' F2
+U S U D
+U M' S2 D M S'
+U F E2 F M'
+.1
+U R2 D2 L2
+L2 F U B2
+B2 L ,5
+U' F D L' S2 R'
+U B' L' M2 U2 E' S
+L2
+2
