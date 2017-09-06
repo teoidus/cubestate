4fbe82776d4982564583ef049348c564f34e2841
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:46:25 -0400

    Create atbash.cs

---
+++ ex/stolen-from-keymaker/atbash.cs
@@ -0,0 +1,35 @@
+R U' ,2
+E' R' B' D R
+R' B R' U2 F'
+L F' L' E F M'
+F M U2 R' U F2 R'
+D U2 B M' U' M2 B'
+B U S M R2 B'
+U M B L R S'
+R L' b2 U' B R'
+F' D' R2 U2 L2 F2
+U2 F2 L2 B2 L'
+U L' B2 D S E' M'
+S M2 F' L U L2
+F' R2 U f' R
+U L B' L D2 B M2
+S E2 M' U F2 R'
+F2 R L2 D L' M2
+B M S' U' E' F2 R
+D' B S2 R2 U' L
+R D' S2 D' S2 M L2
+E' F2 U' S' L F
+l U D2 S R2 F2
+D2 B R' S B2 L2
+l2 U' R' S D' B
+M' D M' U2 R2 B L'
+R' E B2 U S L2
+R U L' S B' R2
+L M2 B L' U L2
+U2 B U M2 D F2
+U B' U' B' R E2 S
+U E2 L2 D' F'
+U2 B .2
+U R L' ,3
+U'
+1
