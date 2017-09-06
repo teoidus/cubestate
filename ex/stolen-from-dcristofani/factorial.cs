e161d09819d5f8b4849c662e79438f4565953c98
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:43:14 -0400

    Create factorial.cs

---
+++ ex/stolen-from-dcristofani/factorial.cs
@@ -0,0 +1,35 @@
+B D' B'
+F E F2 U' S' D2 R'
+U M' D E2 R'
+U L U' b L' U'
+F B U' B' M S
+U B S L2 D2 F2 R2
+R' B2 R' M2 F
+F2 R2 U F L S'
+U D B' R' D2 F M'
+L R2 U S' R' M2 B
+f2 U S D2 B
+U M S' E' L B2 L2
+B E' F L R
+U M2 D R' F U' F
+R' U F2 M B R2
+F R E' F' U r
+R2 D' B' L B2
+U' L F U' D2 S
+R U E D' R
+U R' M2 F D M D2 F'
+D' S' R E M' F2
+U L D' F' M S' B2
+F D2 F' D' B2
+F' R U2 R S F'
+S' L D2 B L' M2
+U' B' U2 L E B
+U S' L' D B M F'
+D L' S D2 L B2
+L D F' U' R S2
+B L E' S2 U2 R'
+B' U R' U' S2 R
+L' U .2
+L R' B' S2 R' .7
+L2 S' B2
+5
