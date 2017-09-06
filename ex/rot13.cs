2a431ea3d728c9cba5a65b1a89d636e245b4d91a
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:00:44 -0400

    Update rot13.cs

--- ex/rot13.cs
+++ ex/rot13.cs
@@ -1,24 +1,24 @@
 L2 ,1
-U B2 S' U M S2
+U S' B2 U M S2
 S' D2 R' D R'
-F' D2 R' F' S' M'
-F2 M B' L2 S R B
-R2 S' D' L F M
-R' S U' D' R F2 R2
-U R L' B S L
-R2 B' S' D' M F
-L' D' R' B2 R' M'
-R D2 S' D' F2 B2
-D' F2 B' U' S L' F2
-B R L2 F' S2 M
-B D2 M2 D B2
-R F M2 F R2 S'
-U S' M2 F' R F
-U2 M F' D R2 L2
-L2 S2 U R' S' M'
-E2 F B R B2
-R L D' B' D S2
-R' F U B U2 M .8
-S' L U ,5
-R B M2
+L F2 M' U' D2 L
+F' L R2 U' l
+U R' F M B2 L F2
+U D2 M S2 U L2 F'
+L U M B' L2 D' B2
+U L S' E' F L R2
+D L2 S D B'
+R F' L U' D2 M'
+U B' U M F R U'
+U M B2 R S' R' F'
+D2 M2 B D' B2
+R2 S2 U R' F S'
+E B' R D' M' B2
+B E' L B' M' F' R
+S' U2 R F' B' M
+F D B R F2
+F D R S2 L B
+U D' F M' S2 L2 .8
+L F' M2 S ,5
+L M2
 2
