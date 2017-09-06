ba127b1cc91373aafb5c5a24bb7f5f397162a8bb
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:52:58 -0400

    Create bfquine.cs

---
+++ ex/stolen-from-keymaker/bfquine.cs
@@ -0,0 +1,46 @@
+F D S L2 S' M' F'
+U B L' U2 M F R
+U M2 D M F2 D' B' F2
+
+B' S' U B2 M
+L2 S D' L2
+E' S2 R2 U2 R' S2
+B M B' M'
+
+F D F'
+F2 D' M S2 U L2
+B' U' B' D M' B L
+E2 S2 R' S2
+U2 S E S'
+U D M2 B M2
+U M F2 M2 D F2 M2
+E2 L' U M B2 R'
+B M' B' U' R
+U S' U2 S
+R' F E' F'
+U L' U2 S' U L F
+U D B' L' M2 B M2
+U M' U2 E' M
+U D' F2 L2 F2
+U F' D2 F
+U E M L' U R2 B2
+R' B' U R U
+l F2 M' E2 L
+D F S' M F'
+L D L' U2
+M B2 U' L2 U2 S
+L U D2 L' B'
+B D2 M' U' L'
+U L B l F L2
+D E2 F' E S R
+M' R2 d2 L' S
+F D' B' D2 R' F2
+B D' B2 E2 L
+S' L' U2 L F'
+B' U2 S R2 F
+S L2 D' B2 L M2 S'
+U D E' F'
+F L' D' L
+U .1
+B2
+2
