a537ad54a8d3265fc049b893b3959aeafbe1df76
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:14:52 -0400

    Create xmastree.cs

---
+++ ex/stolen-from-dcristofani/xmastree.cs
@@ -0,0 +1,18 @@
+L D L2 r2
+L2 U2 ,3
+U E2 L' F2 r2
+L' U2 F' E2 F' R
+E' F' D S2 R B2
+M E' S' F2 R ,6
+L R B2 U' F M S2
+F M D' S2 L2 B2
+B2 U F' L'
+U B l' D2 F'
+U F L S D' M'
+F M' f2 U F
+.1
+E F M' F' R .7
+U B2 U2 L' F' B2 M2
+L U L2 .3
+S D' S B2 .5
+F' M2 .2
