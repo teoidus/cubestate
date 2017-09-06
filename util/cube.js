4732fed2fd1ddf88cd471ecf1923c20798139f16
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-05 20:45:41 -0400

    Update cube.js

--- util/cube.js
+++ util/cube.js
@@ -160,7 +160,6 @@ Cube.prototype.bruteForce = function (goal, moveGroup, maxDepth, depth, accu, tr
 
 			var attempt = this.bruteForce(goal, moveGroup, maxDepth, depth + 1, accu, transpositions);
 			if (attempt)
-				this.apply(move, true);
 				return accu;
 
 			this.apply(move, true);
