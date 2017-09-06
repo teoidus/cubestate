88af0333e1617636968fc660a3186390ecac0161
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:43:46 -0400

    Create utm.cs

---
+++ ex/stolen-from-dcristofani/utm.cs
@@ -0,0 +1,55 @@
+U L F L'
+L M' B2 L' ,4
+U' R' E' B U2 S'
+L B' D L' D' S'
+U R D2 R2 S2 D B
+D R2 S2 U' S D
+U F' D2 R M2 B R2
+L F' E M' U2 F S2
+R F' L' U D' R'
+U R2 B R E F
+M B R2 B2 R F'
+F' U2 F' D E' S'
+U R2 D2 B' U S R2
+S R M F M' L2
+S2 U' M L' D' S'
+U F2 U' L' S
+R E2 B2 R2 B'
+B R' B2 D2 R
+B M U F2 L' R2 F
+F L' S2 D B' M
+S' L F2 R' B R
+U M' D L F M2 S2
+D2 R' E' L F' S2
+D S' U2 F2 D L'
+M S' L2 B' R' F2
+U D' R S' E' F'
+B D2 R' B' L E' M
+L' B' D' B2 M S
+U F L' R' U2 D S
+D' L2 F' r
+M S' R' D F' L'
+D B M2 F R' L2
+U R S U M2 F
+D R M U' M B'
+F M F2 U' L' B
+S' L' F L' B'
+L D S U2 R' M' B2
+R' S M' S2 U' F2
+F' M' F L2
+L B L2 U S l'
+B D B'
+U R D2 F2 R
+F' L B M U2 F
+M' S' U' D' R' S2
+E' S2 D' F R2
+B M S L S2 R2
+D F2 R' S' L' M2
+U L' D2 R2 D S' M'
+B F2 L2 D' M F' R
+F M' B2 R2 U' D' R
+d B2 M B S
+u F' M B' S2 L
+.0
+B L2
+2
