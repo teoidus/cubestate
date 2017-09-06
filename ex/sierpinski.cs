06b7274bad636558305ba4e258abaa0078178d14
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 13:00:03 -0400

    Create sierpinski.cs

---
+++ ex/sierpinski.cs
@@ -0,0 +1,14 @@
+F D2 F'
+L B U' B2 L' B
+F2 U2 M' D' M R2
+U R2 B' D R' S2 M'
+U F D' l S R2
+U F' E B' R M
+U' B D' M2 F2 E L'
+U S2 L2 F R' M' F
+U S M U2 B l2
+F L' D2 L' F2 L2
+U F' L' .6
+U R' B' R' S' .7
+R B' D2 R b2
+9
