5d21e42e534a5695c66a1bda3485f7a4ed12016e
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-06 12:44:31 -0400

    Create quicksort.cs

---
+++ ex/quicksort.cs
@@ -0,0 +1,22 @@
+E' l' S ,8
+L U' L' ,5
+M' U' L2 F L2
+L2 F r F' R2
+L D' E2 F L B' L
+B R L2 D S2
+L D B2 D' L' S2
+U R B L R2 S L2
+F M' u'2 S' D B2
+B R' U2 B S R
+F D S R2 D R2
+b R U R S2
+E S L U' L2 B
+B U d2 B' L' F2
+L F2 M D B' R2 M2
+U S E M' B' R' F2
+L R' D2 F' M2 B2
+S L B S2 L'
+B r U' R2 S'
+B U2 M' .5
+U M' U2
+4
