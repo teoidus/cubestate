a2bdc0155bbe8fa2bfb9d5e331814b406d0e1236
teoidus <18508128+teoidus@users.noreply.github.com>
2017-09-06 10:39:04 -0500

    Additional examples

---
+++ ex/logic.cs
@@ -0,0 +1,70 @@
+L' ,1
+R F' R2 E' B' R2 M
+F' R' B U' R S'
+M D' L2 S U2 F
+R D2 E' B' U2 R S
+R E' L2 F' M' B M2
+R E2 F U' M' B
+U2 R2 M D F' B2 M
+L F2 U' F2 B' L2
+D' B2 R B2
+D2 L B U' F M
+E B M2 D S'
+F' U2 M2 B D F
+R U E B U S' M2
+R M2 F2 L E B
+R2 B L B' D2 S' M'
+R L M' E2 B U' B'
+F' R D2 B' S2 R
+U F2 L' B U' S L'
+M2 D' R2 E F L
+F2 U2 L E' S'
+L' F U2 S M' F'
+F' U R' U S' L2
+R M2 B U2 F2 R' E' S
+S2 M' U D' R
+U' B M E S L B2
+L F2 D' M E S' R2
+E2 S R' U R2 B
+R' U' B2 R' L2 S
+F R B' D' S' L B'
+U B2 S' D' R S2
+F' B' D F' M S2 U R
+R F' U' D' B2 L' M
+R L2 F' R D M' B'
+S U R' M' F'
+F M' F2 R' U' B' M'
+U' F' U2 D2 L' M
+L2 U2 B D2 L' S
+U' S D F2 R
+U' L2 D R F R' S'
+R' S' R .6
+U D2 B U B R2
+L U2 D' S' .6
+L S' M2 B D B2
+R B' D2 B E S
+U D2 R2 L F
+B' U2 B R L U' B
+F R D2 R B S2
+B S2 R2 L' U' R2
+F' S M2 F' R
+R' F L2 B S M
+R' B M' U F' D M'
+L D' F2 M B' M
+F R' D2 L S2 R
+R F2 D' S L2 S
+R2 D L2 B' M' D F2
+R2 U' B' U2 M S
+R M2 S' U' F2 M' S
+F2 B2 U2 M B' L
+U' S' L' F D' R2
+D B' R2 L M2
+L F U F' R2 S2
+U L' U E F' R
+D2 R2 U L' B M'
+M' E' L U2 E' F'
+E L U2 M2 E' L'
+L' F U B' R2 S2
+,0
+U'
+1
---
+++ ex/rot13.cs
@@ -0,0 +1,24 @@
+L2 ,1
+U B2 S' U M S2
+S' D2 R' D R'
+F' D2 R' F' S' M'
+F2 M B' L2 S R B
+R2 S' D' L F M
+R' S U' D' R F2 R2
+U R L' B S L
+R2 B' S' D' M F
+L' D' R' B2 R' M'
+R D2 S' D' F2 B2
+D' F2 B' U' S L' F2
+B R L2 F' S2 M
+B D2 M2 D B2
+R F M2 F R2 S'
+U S' M2 F' R F
+U2 M F' D R2 L2
+L2 S2 U R' S' M'
+E2 F B R B2
+R L D' B' D S2
+R' F U B U2 M .8
+S' L U ,5
+R B M2
+2
