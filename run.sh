d10c3b3d598ad9250af1a193bb59a575c07d26b3
raumaankidwai <raumaankidwai@gmail.com>
2017-09-06 19:56:32 -0400

    .

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 tmp=$(mktemp)
 
-node -e 'console.log(require("./cubestate.js").compile(require("fs").readFileSync("'"$1"'")))' > $tmp
+node -e 'console.log(require("./cubestate.js").compile(require("fs").readFileSync("'"$1"'").toString()))' > $tmp
 
 ./bfi $tmp
 
