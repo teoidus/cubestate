ceb1d1ed960dd56bca0ef56c3631f8cf5e1f8f25
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-09-21 20:21:17 -0400

    Create morse.cs

---
+++ ex/stolen-from-keymaker/morse.cs
@@ -0,0 +1,308 @@
+R U' d' B'
+L F2 U R B E L
+U R2 D2 R' B2 E2 F'
+U' S2 L B2 D E' L
+M S2 D' B R' B D
+U2 M' F2 R ,7
+R B2 D L' E' S
+L2 S' B2 D L U' F
+L D2 L' F' D2 F
+
+L D' R S L' D R' D
+R' D2 R D
+F S' R E' F' R E F
+U B E' M L2 E L D F
+B' L' U S' E2 F'
+F D2 B R2 S' U F R'
+S D R' E2 F' R
+F R' D S R' U E2 F'
+U D B' L' S R2 D' F
+F' U B E' M' F2 R U
+F' R' B M U' D' F'
+F E' S U2 R' F L U
+F S' U B E D2 F
+U R L' .3
+.0
+.1
+.0
+B' .1
+U .1
+L F L' R' S D' F2
+U .1
+.0
+U' .1
+.0
+U' D' B' r' E L2
+.0
+.1
+B .1
+.0
+B M2 U S' L2 R2 F
+U .1
+U' .1
+.0
+U .1
+U L2 U2 D2 R2
+.0
+U .1
+.0
+U' L D2 L2 D
+.0
+.0
+.0
+U .1
+U' L E2 L' D
+.0
+.0
+U .1
+U' L F' D' F
+U .1
+U B2 U2 D2 F2
+.0
+.0
+.0
+R' S U' D' F2 R .8
+B .1
+L' .1
+L F B' D' F' D
+.0
+.0
+.1
+B .1
+D M B D' S' D' L'
+.0
+U .1
+.0
+L .1
+B M F' S2 L D L
+.0
+.0
+.0
+M' F U R F' D2 R
+U .1
+U' .1
+L M2 B' E S' D L2
+.0
+.0
+L B' D' M2 S' E' L'
+.0
+U .1
+L .1
+.0
+F' R M2 E S D L'
+.0
+.1
+U2 .1
+R2 B2 D' M S L F2
+.0
+U .1
+.0
+.0
+U' L D2 L' D
+.0
+.0
+R D S2 M' E2 F .8
+.0
+.0
+.0
+F' D F'
+.0
+.0
+.1
+.8
+.0
+U2 .1
+L .1
+U2 R' E2 R' .8
+
+.0
+.1
+.0
+
+.0
+.1
+B' .1
+B .1
+
+.0
+.1
+.0
+.0
+.8
+B' .1
+L R' E S' L2 D' L F2
+.0
+U .1
+.0
+U' .1
+U .1
+U' .1
+S U' R' F2 B2 R' .8
+.0
+L' .1
+.0
+L .1
+.0
+F D2 F'
+.0
+.1
+.0
+.0
+L' .1
+D2 M S' E' B D
+U .1
+U' .1
+U .1
+U' .1
+U .1
+U' .1
+B' D' M' S D2 L2
+.0
+.0
+.0
+.1
+.0
+.0
+
+.0
+.0
+.0
+.0
+.1
+
+.0
+.0
+.0
+.1
+.0
+
+.0
+.0
+.1
+.0
+.0
+
+.0
+.1
+.0
+.0
+.0
+.8
+.0
+.0
+.0
+.0
+.8
+.0
+.0
+.0
+U2 .1
+S M' U2 L2 S2 D' B
+.0
+.0
+.0
+U .1
+.0
+U' L E' L' D
+.0
+.0
+U .1
+.0
+.0
+U' L D' L' D
+.0
+U .1
+.0
+.0
+.0
+U' L E L' D
+U .1
+.0
+.0
+.0
+.0
+U' L E' L' D
+U .1
+U' .1
+.0
+U .1
+U' .1
+F R2 S' M D' F2 .8
+B .1
+B' .1
+B .1
+B' .1
+B .1
+F U2 S E' B' E R'
+U .1
+L .1
+.0
+.0
+.0
+L' .1
+U' L B L' D
+U .1
+.0
+U' .1
+.0
+U .1
+.0
+U' R E R' D
+U .1
+U' .1
+U .1
+.0
+U' .1
+U .1
+U' F E L' F'
+U .1
+L .1
+L' .1
+.0
+L .1
+L' .1
+L U' E' F' D
+.0
+U .1
+.0
+.0
+.0
+L .1
+d M' S R' F2 .8
+R U2 .2
+B U' .2
+.0
+.0
+D U' E2 F' R .8
+B' .1
+B .1
+.0
+B' .1
+B .1
+
+.0
+.1
+B' .1
+B .1
+B' .1
+.0
+R F U S2 r D
+B .2
+.0
+.0
+.0
+.0
+M' F B' L' F2 E R2
+R' B .2
+B' .1
+.0
+.0
+B .1
+B' E2 R' D S' D' F
+U .1
+L D2 M2 ,4
+F B2 M' .3
+U' .1
+L2 .1
+B .1
+U .1
